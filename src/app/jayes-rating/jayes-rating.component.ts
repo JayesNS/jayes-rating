189818de2f9b3735c6f02b35a9611f585c02c92f
Jayes <jakubszkarlat@gmail.com>
2018-03-04 21:38:45 +0100

    Added basic UI and star amount calculating

--- src/app/jayes-rating/jayes-rating.component.ts
+++ src/app/jayes-rating/jayes-rating.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {containerStart} from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'jayes-rating',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jayes-rating.component.css']
 })
 export class JayesRatingComponent implements OnInit {
+  @Input() value: number;
 
-  constructor() { }
+  @Input() maxValue: number;
 
-  ngOnInit() {
+  public stars: any[];
+
+  constructor() {
+    this.value = 0;
+    this.maxValue = 10;
+
+    this.stars = new Array(this.maxValue);
   }
 
+  ngOnInit() {
+    this.stars.fill(StarType.EMPTY_STAR);
+    this.calculateStars();
+  }
+
+  calculateStars() {
+    let value = this.value;
+    let starType = StarType.EMPTY_STAR;
+
+    this.stars = this.stars.map(star => {
+      if (value >= 0.75) {
+        starType = StarType.FULL_STAR;
+      } else if (value >= 0.25) {
+        starType = StarType.HALF_STAR;
+      } else {
+        starType = StarType.EMPTY_STAR;
+      }
+
+      --value;
+      return starType;
+    });
+  }
 }
+
+export const StarType = {'EMPTY_STAR': 'star_border', 'HALF_STAR': 'star_half', 'FULL_STAR': 'star'};
