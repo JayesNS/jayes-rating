9f7f32a74ae13f8579058b855df708a2091e74d6
Jayes <jakubszkarlat@gmail.com>
2018-03-07 09:41:36 +0100

    Automatically displaying diffrent versions of ratings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
+  public mockRatings = [
+    {values: [], maxValue: 10, disabled: false, rateSubmit: null},
+    {values: [2.24999999999999], maxValue: 10, disabled: false, rateSubmit: null},
+    {values: [4.6], maxValue: 10, disabled: false, rateSubmit: null},
+    {values: [15], maxValue: 5, disabled: false, rateSubmit: this.saveRate},
+    {values: [-1], maxValue: 10, disabled: false, rateSubmit: null},
+    {values: [1.1], maxValue: 10, disabled: true, rateSubmit: null}
+  ];
+
   saveRate() {
     console.log('Here you can save rateSubmit');
   }
