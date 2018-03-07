736a5db4b38364fc55354c35ddbc97743e3f211c
Jayes <jakubszkarlat@gmail.com>
2018-03-07 13:26:09 +0100

    Added basic style customization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,13 @@ export class AppComponent {
   title = 'app';
 
   public mockRatings = [
-    {values: [], maxValue: 10, disabled: false, rateSubmit: null},
-    {values: [2.24999999999999], maxValue: 10, disabled: false, rateSubmit: null},
-    {values: [4.6], maxValue: 10, disabled: false, rateSubmit: null},
-    {values: [15], maxValue: 5, disabled: false, rateSubmit: this.saveRate},
-    {values: [-1], maxValue: 10, disabled: false, rateSubmit: null},
-    {values: [1.1], maxValue: 10, disabled: true, rateSubmit: null}
+    {desc: 'No values', values: [], maxValue: 10, disabled: false, rateSubmit: null},
+    {desc: 'Rounding', values: [2.24999999999999], maxValue: 10, disabled: false, rateSubmit: null},
+    {desc: 'Mulitple values', values: [4.6, 5.6], maxValue: 10, disabled: false, rateSubmit: null},
+    {desc: 'Out of range (too high)', values: [15], maxValue: 5, disabled: false, rateSubmit: this.saveRate},
+    {desc: 'Out of range (too low)', values: [-1], maxValue: 10, disabled: false, rateSubmit: null},
+    {desc: 'Disabled', values: [1.1], maxValue: 10, disabled: true, rateSubmit: null},
+    {desc: 'Customized class', values: [1.0001], maxValue: 10, disabled: false, rateSubmit: null, class: 'color-red'}
   ];
 
   saveRate() {
